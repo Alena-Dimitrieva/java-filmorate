06ea66802a432572016f72a89feadc8c1984953c
Alena <alena.d980327@gmail.com>
2025-12-12 22:29:34 +0300

    отладка для чекстайла

--- src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
@@ -1,20 +1,48 @@
 package ru.yandex.practicum.filmorate.exception;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+@SuppressWarnings("unused")
 @RestControllerAdvice
 public class ErrorHandler {
 
-    @ExceptionHandler
-    public ResponseEntity<String> handleValidation(ValidationException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    // Обработка кастомной валидации
+    @ExceptionHandler(ValidationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationException(ValidationException e) {
+        return Map.of("error", e.getMessage());
     }
 
-    @ExceptionHandler
-    public ResponseEntity<String> handleOther(Exception e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    // Обработка стандартной валидации Spring (например, @NotBlank, @Email, @PastOrPresent)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationErrors(MethodArgumentNotValidException e) {
+        String errorMessage = e.getBindingResult().getFieldErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage) //сообщение из аннотаций
+                .collect(Collectors.joining("; "));
+        return Map.of("error", errorMessage);
+    }
+
+    // Обработка случая, когда объект не найден (например, пользователь/фильм по id)
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleNotFound(NoSuchElementException e) {
+        return Map.of("error", e.getMessage());
+    }
+
+    // Общая обработка всех остальных исключений
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleOtherExceptions(RuntimeException e) {
+        return Map.of("error", e.getMessage());
     }
 }
